782fecb8c405caeef3c81df109dacc7df9361401
Nicolas <nicolas.lorrayne@gmail.com>
2021-03-10 23:40:14 -0300

    Select no addArtigo

--- frontend/addArtigo.php
+++ frontend/addArtigo.php
@@ -8,32 +8,32 @@
 </head>
 <body>
     <div class="d-flex vh-100">
-        <div class='w-50 d-flex justify-content-center align-items-center flex-column'>
-            <h1 id='cad' class='h1 fw-bolder w-25 ps-3 mb-1'>CADASTRO DE USUÁRIO</h1>
-            <p id='atual'class='ps-5'>cadastre-se, atualize-se</p>
-        </div>
-        <div class='w-50 d-flex justify-content-center align-items-center'>
-            <form enctype='multipart/form-data' action='../backend/action/inserir.php' method='post' class='w-75'>
+        <div class='w-100 d-flex justify-content-center align-items-center flex-column'>
+            <h1 id='cad' class='h2 fw-bolder ps-3 mb-2'>CADASTRO DE ARTIGOS</h1>
+            <form enctype='multipart/form-data' action='../backend/action/inserir.php' method='post'>
                 <div class='form-div form-floating mb-3'>
                     <input type='text' class='form-control' id='titulo' name='titulo' placeholder='Título' style='outline: none;' required>
                     <label for='titulo'>Título</label>
                 </div>
-                <div class='form-div form-floating mb-3'>
-                    <input type='email' class='form-control' id='natureza' name='natureza' placeholder='Natureza' required>
-                    <label for='natureza'>Natureza</label>
+                <div>
+                    <select class="form-select mb-3" name='natureza'>
+                        <option selected>RESUMO</option>
+                        <option value="2">COMPLETO</option>
+                    </select>
                 </div>
                 <div class='form-div form-floating mb-3'>
-                    <input type='password' class='form-control' id='autPrinc' name='autPrinc' placeholder='Autor principal' required>
+                    <input type='text' class='form-control' id='autPrinc' name='autPrinc' placeholder='Autor principal' required>
                     <label for='autPrinc'>Autor principal</label>
                 </div>
                 <div class='form-div form-floating mb-3'>
-                    <input type='password' class='form-control' id='emailAutPrinc' name='emailAutPrinc' placeholder='Email do autor principal' required>
+                    <input type='email' class='form-control' id='emailAutPrinc' name='emailAutPrinc' placeholder='Email do autor principal' required>
                     <label for='emailAutPrinc'>Email do autor principal</label>
                 </div>
                 <div>
                     <button type="submit" class='w-100 float-end btn btn-primary'>
                         <i class="fas fa-user-plus"></i>   CADASTRAR NOVO ARTIGO
                     </button>
+                    <a href='registrar.php' class='mt-2 btn btn-secondary w-100' title='Home'><i class='fas fa-home'></i> HOME</a>
                     <a href='userLista.php' class='btn btn-outline-secondary w-100 mt-2' title='Função disponível apenas para administradores'>
                         <i class="fas fa-users"></i>   LISTA DE ARTIGOS
                     </a>     
--- frontend/addCoaut.php
+++ frontend/addCoaut.php
@@ -7,31 +7,28 @@
     <link rel="stylesheet" href="css/simpleAnimation.css">
 </head>
 <body>
-    <div class="d-flex vh-100">
-        <div class='w-50 d-flex justify-content-center align-items-center flex-column'>
-            <h1 id='cad' class='h1 fw-bolder w-25 ps-3 mb-1'>CADASTRO DE USUÁRIO</h1>
-            <p id='atual'class='ps-5'>cadastre-se, atualize-se</p>
-        </div>
-        <div class='w-50 d-flex justify-content-center align-items-center'>
-            <form enctype='multipart/form-data' action='../backend/action/inserir.php' method='post' class='w-75'>
+<div class="d-flex vh-100">
+        <div class='w-100 d-flex justify-content-center align-items-center flex-column'>
+            <h1 id='cad' class='h2 fw-bolder ps-3 mb-2'>CADASTRO DE CO-AUTORES</h1>
+            <form enctype='multipart/form-data' action='../backend/action/inserir.php' method='post'>
                 <div class='form-div form-floating mb-3'>
-                    <input type='text' class='form-control' id='userNome' name='nome' placeholder='Nome para usuário' style='outline: none;' required>
-                    <label for='userNome'>Nome de usuário</label>
+                    <input type='text' class='form-control' id='titulo' name='titulo' placeholder='Título' style='outline: none;' required>
+                    <label for='titulo'>Código do artigo</label>
                 </div>
                 <div class='form-div form-floating mb-3'>
-                    <input type='email' class='form-control' id='userEmail' name='email' placeholder='nome@exemplo.com' required>
-                    <label for='userEmail'>Endereço de e-mail</label>
+                    <input type='text' class='form-control' id='natureza' name='natureza' placeholder='Natureza' required>
+                    <label for='natureza'>Nome do co-autor</label>
                 </div>
                 <div class='form-div form-floating mb-3'>
-                    <input type='password' class='form-control' id='userSenha' name='senha' placeholder='senha' required>
-                    <label for='userSenha'>Senha</label>
+                    <input type='email' class='form-control' id='autPrinc' name='autPrinc' placeholder='Autor principal' required>
+                    <label for='autPrinc'>Email do co-autor</label>
                 </div>
                 <div>
                     <button type="submit" class='w-100 float-end btn btn-primary'>
-                        <i class="fas fa-user-plus"></i>   CADASTRAR
+                        <i class="fas fa-user-plus"></i>   CADASTRAR NOVO CO-AUTOR
                     </button>
                     <a href='userLista.php' class='btn btn-outline-secondary w-100 mt-2' title='Função disponível apenas para administradores'>
-                        <i class="fas fa-users"></i>   LISTA DE USUÁRIOS
+                        <i class="fas fa-users"></i>   LISTA DE CO-AUTORES
                     </a>     
                 </div>   
             </form>
