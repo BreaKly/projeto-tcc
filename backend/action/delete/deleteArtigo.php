4b459302a7969b2ddfce9dcfdbd0658adb50aab9
Nicolas <nicolas.lorrayne@gmail.com>
2021-03-11 22:47:03 -0300

    Função deletar Artigo update

--- backend/action/delete/deleteArtigo.php
+++ backend/action/delete/deleteArtigo.php
@@ -1,8 +1,8 @@
 <?php
-    require_once('../Conexao/Conexao.class.php');
-    require_once('../modelo/artigoModel.php');
+    require_once('../../Conexao/Conexao.class.php');
+    require_once('../../modelo/artigoModel.php');
     try {
-        $conn = new Conexao("../Conexao/configDB.ini");
+        $conn = new Conexao("../../Conexao/configDB.ini");
         $pdo = $conn->getPDO();
         
         $codArtigo = $_GET['codArtigo'];
@@ -12,7 +12,7 @@
         if($comando->execute()) {
             header("refresh:0, ../../frontend/listaGeral");
         } else {
-            echo "A conta do usuário ".$codProj." não foi excluída devido a problemas inesperados.";
+            echo "O Artigo ".$codArtigo." não foi excluída devido a problemas inesperados.";
         }
 
 
