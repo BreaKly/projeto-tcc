9616fd79aabf737b69d36ca1848683273c767afc
Emanuel <Manuibra327@gmail.com>
2021-03-11 23:39:30 -0300

    FINALIZADO

--- backend/Conexao/Conexao.class.php
+++ backend/Conexao/Conexao.class.php
@@ -1,32 +1,15 @@
 <?php
 class Conexao{
-   /* Definição dos atributos para conectar ao SGBD */
    private $usr;
    private $pwd;
    private $server;
    private $db;
    private $driver;
    private $pdo;
-   
-   /* Sem encapsulamento pois todos os dados devem ser manipulados somente nesta classe */
-
-   /* Construtor
-    * Abre a conexão com o SGBD 
-    * Receber um arquivo XML ou INI e preencher atributos da classe 
-    * Usar PDO com os atributos da classe para efetuar conexão */
       function __construct($arq){
          try{
-            /* file_exists(arq)
-             * Verifica se arq existe
-             * retorna boolean
-             * */
             if(file_exists($arq)){
-               /* parse_ini_file(arq)
-                * Recebe as informações de arquivo ini
-                * Retorna Array associativo 
-                */
                $config = parse_ini_file($arq);
-               /* Populando objeto da classe */
                $this->usr = $config['usuario'];
                $this->pwd = $config['password'];
                $this->server = $config['host'];
@@ -37,12 +20,6 @@ class Conexao{
                      $this->pdo = new PDO("{$this->driver}:host={$this->server};dbname={$this->db}",$this->usr,$this->pwd);
                      $this->getPDO()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                      echo "<script>console.log('Conexão efetuada com sucesso');</script>";
-                     break;
-                  case "pgsql":
-                     // Código do postgre
-                     break;
-                  case "oracle":
-                     // Código do oracle
                      break;
                   default:
                      die("<script>console.error('SGBD Incompatível com o sistema');</script>");  
--- frontend/home.php
+++ frontend/home.php
@@ -9,7 +9,7 @@
 <body>
     <div class="d-flex vh-100">
         <div id='title-box' class='w-50 d-flex justify-content-center align-items-center'>
-            <div id='title-text' class='justify-content-center align-items-center flex-column overflow-hidden' style=''>
+            <div id='title-text' class='justify-content-center align-items-center flex-column overflow-hidden'>
                 <h1 id='cad' class='h1 fw-bolder m-0' style='width:220px; animation:cad 1.4s ease;'>CADASTRO DE TCC</h1>
                 <p id='atual'>cadastre-o, atualize-o.</p>
             </div>
@@ -20,34 +20,7 @@
             <a class='btn btn-primary w-50 m-1' href='addArtigo.php'>Artigos</a>
             <a class='btn btn-primary w-50 m-1' href='addProjeto.php'>Projetos</a>
             <a class='btn btn-primary w-50 m-1' href='addCoaut.php'>Co-Autor</a>
-            
-
-            
         </div>
-        <!-- <div class='w-50 d-flex justify-content-center align-items-center'>
-            <form enctype='multipart/form-data' action='../backend/action/inserir.php' method='post' class='w-75'>
-                <div class='form-div form-floating mb-3'>
-                    <input type='text' class='form-control' id='userNome' name='nome' placeholder='Nome para usuário' style='outline: none;' required>
-                    <label for='userNome'>Nome de usuário</label>
-                </div>
-                <div class='form-div form-floating mb-3'>
-                    <input type='email' class='form-control' id='userEmail' name='email' placeholder='nome@exemplo.com' required>
-                    <label for='userEmail'>Endereço de e-mail</label>
-                </div>
-                <div class='form-div form-floating mb-3'>
-                    <input type='password' class='form-control' id='userSenha' name='senha' placeholder='senha' required>
-                    <label for='userSenha'>Senha</label>
-                </div>
-                <div>
-                    <button type="submit" class='w-100 float-end btn btn-primary'>
-                        <i class="fas fa-user-plus"></i>   CADASTRAR
-                    </button>
-                    <a href='userLista.php' class='btn btn-outline-secondary w-100 mt-2' title='Função disponível apenas para administradores'>
-                        <i class="fas fa-users"></i>   LISTA DE USUÁRIOS
-                    </a>     
-                </div>   
-            </form>
-        </div> -->
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js"></script>
 </body>
